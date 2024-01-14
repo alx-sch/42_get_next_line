b87f9496de7d3cfcf08a74bca3119898b4b49ddd
Alex Schenk <134595144+alx-sch@users.noreply.github.com>
2024-01-14 17:28:44 +0100

    Update get_next_line_utils.c

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line_bonus.h"
+#include "get_next_line.h"
 
 /*
 As included in the libft project/library.

e45efe4b79a670392445078f97163b127f63cdbd
Alexander Schenk <alexander.o.schenk@gmail.com>
2024-01-16 15:36:32 +0100

    removed not needed function prototypes from header file

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: aschenk <aschenk@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/11 14:02:24 by aschenk           #+#    #+#             */
-/*   Updated: 2024/01/15 12:11:34 by aschenk          ###   ########.fr       */
+/*   Updated: 2024/01/16 15:33:55 by aschenk          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,11 +22,9 @@
 # include <unistd.h> // read()
 # include <stddef.h> // size_t
 
-char	*ft_stash_buf_join(char *stash, char *buffer);
-char	*ft_strchr(const char *s, int c);
-char	*ft_trim_until_newline(char *stash);
-char	*ft_readf(int fd, char *stash);
-char	*ft_getline(char *stash);
+char	*ft_stash_buf_join(char *stash, char *buffer); // in utils
+char	*ft_strchr(const char *s, int c); // in utils
+char	*ft_trim_until_newline(char *stash); // in utils
 char	*get_next_line(int fd);
 
 #endif
--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -6,7 +6,7 @@
 /*   By: aschenk <aschenk@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/11 14:02:24 by aschenk           #+#    #+#             */
-/*   Updated: 2024/01/15 12:19:55 by aschenk          ###   ########.fr       */
+/*   Updated: 2024/01/16 15:34:32 by aschenk          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,11 +31,9 @@
 # include <unistd.h> // read()
 # include <stddef.h> // size_t
 
-char	*ft_stash_buf_join(char *stash, char *buffer);
-char	*ft_strchr(const char *s, int c);
-char	*ft_trim_until_newline(char *stash);
-char	*ft_readf(int fd, char *stash);
-char	*ft_getline(char *stash);
+char	*ft_stash_buf_join(char *stash, char *buffer); // in utils
+char	*ft_strchr(const char *s, int c); // in utils
+char	*ft_trim_until_newline(char *stash); // in utils
 char	*get_next_line(int fd);
 
 #endif
